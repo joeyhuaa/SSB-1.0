677b4db2ef6bd2ce58776dd663caedddba48c4c3
joeyhuaa <joeyhua17@gmail.com>
2019-08-12 23:20:31 -0700

    restored contact.php

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -1,1 +1,15 @@
 <?php
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject  = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "mike@ssbbasketball.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "New message from ".$name."\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.php?mailsend");
+  }
+?>
