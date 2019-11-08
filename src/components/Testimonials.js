338e86ead4c744a27c83922394b84099c5dd6af6
joeyhuaa <joeyhua17@gmail.com>
2019-11-08 08:39:46 -0800

    testimonials font

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Review = (props) => {
   return (
     <div style={{'marginBottom':'7em'}}>
-      <h3>{props.review}</h3>
+      <h3 style={{'fontFamily': 'Love Ya Like A Sister'}}>{props.review}</h3>
       <span style={{'float':'right'}}>{props.name}</span>
     </div>
   )
@@ -14,15 +14,15 @@ function Testimonials() {
     <div className='white-contain'>
       <Review 
         review = '"MY CHILD ALWAYS LOOKS FORWARD TO THE NEXT CLASS. MIKE IS VERY SKILLED AND PATIENT. I HIGHLY RECOMMEND HIM."'
-        name = 'SREE C.'
+        name = '-SREE C.'
       />
       <Review 
         review = '"MIKE IS A FLEXIBLE, ENTHUSIASTIC AND FRIENDLY COACH THAT YOU WOULD LOVE TO HAVE FOR YOUR KIDS…"'
-        name = 'MONICA T.'
+        name = '-MONICA T.'
       />
       <Review 
         review = '"IT MAKES MY DAY SEEING HOW HAPPY AND ACCOMPLISHED DYLAN AND NATE ARE WHEN I PICK THEM UP…"'
-        name = 'BEN C.'
+        name = '-BEN C.'
       />
     </div>
   )
